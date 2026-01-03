6b8427f7ce2b16ad98d789aee3cbe9c7e629b1e3
Zion <zionnoiz1991@gmail.com>
2026-01-03 04:22:03 +0200

    some fixes

--- src/GameListener.App/Program.cs
+++ src/GameListener.App/Program.cs
@@ -3,6 +3,11 @@ using GameListener.App.Services;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using NetCord.Gateway;
+using NetCord.Hosting.Gateway;
+using NetCord.Hosting.Services;
+using NetCord.Hosting.Services.Commands;
+using NetCord.Services;
 
 var host = Host.CreateDefaultBuilder(args)
     .ConfigureAppConfiguration((context, config) =>
@@ -14,10 +19,19 @@ var host = Host.CreateDefaultBuilder(args)
     .ConfigureServices((context, services) =>
     {
         services.Configure<DiscordOptions>(context.Configuration.GetSection("Discord"));
-        services.AddSingleton<BotClientFactory>();
         services.AddSingleton<RecordingManager>();
-        services.AddHostedService<BotHostedService>();
+        services.AddHostedService<BotHostedService>(); 
+        services
+            .AddDiscordGateway(options =>
+            {
+                options.Intents = GatewayIntents.All;
+            })
+            .AddGatewayHandlers(typeof(Program).Assembly)
+            .AddCommands();
     })
     .Build();
+
+// Add commands from modules
+host.AddModules(typeof(Program).Assembly);
 
 await host.RunAsync();
